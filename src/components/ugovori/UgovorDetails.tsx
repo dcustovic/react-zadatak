de90a798fe56192ceb418e9463996aced483afc2
Dominik <domcus44@gmail.com>
2024-03-24 20:09:03 +0100

    ADDED - Ugovor Details design

--- src/components/ugovori/UgovorDetails.tsx
+++ src/components/ugovori/UgovorDetails.tsx
@@ -1,15 +1,18 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { ArtiklType, StatusType, UgovorType } from "../../types.ts";
 import { CircularProgress } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
 
 function UgovorDetails() {
   const { id } = useParams<{ id: string }>();
   const [ugovor, setUgovor] = useState<UgovorType>();
   const [artikli, setArtikli] = useState<ArtiklType[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const navigate = useNavigate();
 
   const getUgovor = async () => {
     setIsLoading(true);
@@ -54,44 +57,63 @@ function UgovorDetails() {
     .reverse()
     .join("-");
 
-  // Filter artikli based on broj_ugovora
+  // Filter artikli u odnosu na broj_ugovora
   const filteredArtikli = artikli.filter((artikal: ArtiklType) => {
     if (artikal && ugovor) {
       return artikal.broj_ugovora === ugovor.broj_ugovora;
     }
   });
+
+  const handleDeleteUgovora = async () => {
+    try {
+      await axios.delete(`http://localhost:4000/ugovori/${id}`);
+      navigate(-1);
+    } catch (error) {
+      // TODO: error handling
+      console.log(error);
+    }
+  };
+
+  const handleEditUgovora = async () => {};
+
   return (
     <div className="flex justify-center items-center my-10">
       {isLoading ? (
         <CircularProgress />
       ) : ugovor ? (
-        <div>
-          <section
-            key={ugovor.id}
-            className="bg-gray-400 rounded-xl mb-5 pt-5 px-16 border-2 border-gray-400 hover:border-blue-400"
-          >
-            <div className="mb-4 font-semibold text-lg">Detalji ugovora</div>
+        <div className="bg-gray-200 rounded-xl mb-5 pt-5 px-16 border-2 border-gray-300">
+          <div className="mb-4 font-semibold text-lg flex justify-between items-center">
+            <div>Detalji ugovora</div>
+            <div className="flex">
+              <button onClick={handleDeleteUgovora}>
+                <DeleteIcon style={{ color: "red" }} />
+              </button>
+              <button onClick={handleEditUgovora}>
+                <EditIcon style={{ color: "blue" }} />
+              </button>
+            </div>
+          </div>
 
-            <div className="font-normal ">
-              <b className="font-semibold">Kupac: </b>
-              {ugovor.kupac}
-            </div>
-            <div className="font-normal">
-              <b className="font-semibold">Broj ugovora: </b>
-              {ugovor.broj_ugovora}
-            </div>
-            <div className="font-normal">
-              <b className="font-semibold">Datum akontacije: </b>
-              {ugovor.datum_akontacije}
-            </div>
-            <div className="font-normal">
-              <b className="font-semibold">Rok isporuke: </b>
-              {format_rok_isporuke}
-            </div>
-            <div className="font-normal">
-              <b className="font-semibold">Status: </b>
-              <span
-                className={`
+          <div className="font-normal ">
+            <b className="font-semibold">Kupac: </b>
+            {ugovor.kupac}
+          </div>
+          <div className="font-normal">
+            <b className="font-semibold">Broj ugovora: </b>
+            {ugovor.broj_ugovora}
+          </div>
+          <div className="font-normal">
+            <b className="font-semibold">Datum akontacije: </b>
+            {ugovor.datum_akontacije}
+          </div>
+          <div className="font-normal">
+            <b className="font-semibold">Rok isporuke: </b>
+            {format_rok_isporuke}
+          </div>
+          <div className="font-normal">
+            <b className="font-semibold">Status: </b>
+            <span
+              className={`
              ${
                ugovor.status === StatusType.KREIRANO
                  ? "text-green-500"
@@ -102,13 +124,12 @@ function UgovorDetails() {
                  : ""
              }
            `}
-              >
-                {ugovor.status}
-              </span>
-            </div>
+            >
+              {ugovor.status}
+            </span>
+          </div>
 
-            <br />
-          </section>
+          <br />
 
           <section>
             {filteredArtikli.map((artikal: ArtiklType) => {
@@ -155,6 +176,13 @@ function UgovorDetails() {
               );
             })}
           </section>
+          <button
+            type="button"
+            onClick={() => navigate(-1)}
+            className="mb-5 mx-1.5 px-4 py-2 text-sm font-medium rounded-md text-white bg-blue-500 hover:bg-blue-700"
+          >
+            Nazad
+          </button>
         </div>
       ) : (
         <div>Nisu pronađeni detalji ugovora.</div>
